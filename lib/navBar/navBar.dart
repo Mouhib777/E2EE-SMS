cc7a1dd69e29d896cdb7ff00c01fa4439ce24e8c
Mouhib777 <mouhibsahbani@gmail.com>
2023-07-02 01:07:30 +0000

    c

---
+++ lib/navBar/navBar.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
+import 'package:sms_encry/constant/constant.dart';
+//import 'package:stream_chat_flutter/stream_chat_flutter.dart';
+
+class navBar extends StatefulWidget {
+  @override
+  State<navBar> createState() => _navBarState();
+}
+
+class _navBarState extends State<navBar> {
+  @override
+  Widget build(BuildContext context) {
+    PersistentTabController _controller;
+
+    _controller = PersistentTabController(initialIndex: 0);
+    List<Widget> _buildScreens() {
+      return [];
+    }
+
+    List<PersistentBottomNavBarItem> _navBarsItems() {
+      return [
+        PersistentBottomNavBarItem(
+          iconSize: 32,
+          icon: Icon(Icons.no_encryption),
+          // title: ("Home"),
+          activeColorPrimary: primaryColor,
+
+          inactiveColorPrimary: Colors.black,
+        ),
+        PersistentBottomNavBarItem(
+          icon: Icon(Icons.no_encryption),
+          // title: ("My Ordre"),
+          activeColorPrimary: primaryColor,
+          inactiveColorPrimary: Colors.black,
+        ),
+        PersistentBottomNavBarItem(
+          iconSize: 35,
+          icon: Icon(Icons.no_encryption),
+
+          //title: ("Favorites"),
+          activeColorPrimary: primaryColor,
+          inactiveColorPrimary: Colors.yellow,
+        ),
+      ];
+    }
+
+    return Scaffold(
+        body: PersistentTabView(
+      context,
+      navBarHeight: 70,
+      controller: _controller,
+      screens: _buildScreens(),
+      items: _navBarsItems(),
+      confineInSafeArea: true,
+      backgroundColor: Colors.white,
+
+      //, // Default is Colors.white.
+      handleAndroidBackButtonPress: true, // Default is true.
+      resizeToAvoidBottomInset:
+          true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
+      stateManagement: true, // Default is true.
+      hideNavigationBarWhenKeyboardShows:
+          true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+      decoration: NavBarDecoration(
+        borderRadius: BorderRadius.only(
+            topRight: Radius.circular(25), topLeft: Radius.circular(25)),
+
+        //border: Border.all(color: Colors.black26)
+      ),
+      popAllScreensOnTapOfSelectedTab: true,
+      popActionScreens: PopActionScreensType.all,
+      itemAnimationProperties: ItemAnimationProperties(
+        // Navigation Bar's items animation properties.
+        duration: Duration(milliseconds: 200),
+        curve: Curves.ease,
+      ),
+      screenTransitionAnimation: ScreenTransitionAnimation(
+        // Screen transition animation on change of selected tab.
+        animateTabTransition: true,
+        curve: Curves.ease,
+        duration: Duration(milliseconds: 200),
+      ),
+      navBarStyle:
+          NavBarStyle.style15, // Choose the nav bar style with this property.
+    ));
+  }
+}
