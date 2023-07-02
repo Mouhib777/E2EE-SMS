e86d7e010d30268b81a88ca09a31e321751ffed0
Mouhib777 <mouhibsahbani@gmail.com>
2023-07-02 01:00:59 +0000

    c

--- lib/screens/contactScreen.dart
+++ lib/screens/contactScreen.dart
@@ -1,4 +1,6 @@
+import 'package:contacts_service/contacts_service.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class contactSCreen extends StatefulWidget {
   const contactSCreen({super.key});
@@ -8,12 +10,58 @@ class contactSCreen extends StatefulWidget {
 }
 
 class _contactSCreenState extends State<contactSCreen> {
+  List<Contact> _contacts = [];
+  @override
+  void initState() {
+    super.initState();
+    fetchContacts();
+  }
+
+  void fetchContacts() async {
+    if (await Permission.contacts.request().isGranted) {
+      Iterable<Contact> contacts = await ContactsService.getContacts();
+
+      setState(() {
+        _contacts = contacts.toList();
+      });
+    } else {
+      // Handle permissions not granted
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("All contacts"),
         centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: _contacts.length,
+        itemBuilder: (context, index) {
+          Contact contact = _contacts[index];
+          return ListTile(
+            leading: (contact.avatar == null && contact.avatar!.isEmpty)
+                ? CircleAvatar(
+                    backgroundImage: AssetImage("assets/images/contact.png"),
+                  )
+                : CircleAvatar(child: Text(contact.initials())),
+            title: Text(
+              contact.displayName ?? '',
+              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text(
+              contact.phones!.isNotEmpty
+                  ? contact.phones!.first.value ?? ''
+                  : '',
+              style: TextStyle(fontSize: 14.0),
+            ),
+            trailing: Icon(Icons.phone),
+            onTap: () {
+              // Handle contact tap
+            },
+          );
+        },
       ),
     );
   }
