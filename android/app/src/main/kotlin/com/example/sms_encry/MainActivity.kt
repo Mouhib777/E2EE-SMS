26fa7fbe24f95fb1cd60855965545e8980104f39
Mouhib777 <mouhibsahbani@gmail.com>
2023-07-02 16:16:19 +0000

    c

--- android/app/src/main/kotlin/com/example/sms_encry/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/sms_encry/MainActivity.kt
@@ -1,34 +1,45 @@
 package com.example.sms_encry
+
 import android.content.Intent
 import android.provider.Telephony
+import android.os.Bundle
 import androidx.annotation.NonNull
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 
-import io.flutter.embedding.android.FlutterFragmentActivity
-class MainActivity: FlutterFragmentActivity() {
+class MainActivity : FlutterFragmentActivity() {
     private val CHANNEL = "com.example.sms_encry"
 
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
+
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
             if (call.method == "setDefaultSms") {
                 try {
-                    var intent = Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT);
-                    intent.putExtra(
-                            Telephony.Sms.Intents.EXTRA_PACKAGE_NAME,
-                            "com.example.sms_encry"
-                    );
-                    startActivity(intent);
-                    result.success("Success")
+                    val intent = Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT)
+                    intent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME, packageName)
+                    startActivity(intent)
+                    result.success("Success t7atet app as default SMS app")
                 } catch (ex: Exception) {
                     result.error("UNAVAILABLE", "Setting default sms.", null)
                 }
             } else {
                 result.notImplemented()
             }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Check if the app is already set as the default SMS app
+        if (Telephony.Sms.getDefaultSmsPackage(this) != packageName) {
+            // Prompt the user to make this app the default SMS app
+            val intent = Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT)
+            intent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME, packageName)
+            startActivity(intent)
         }
     }
 }
