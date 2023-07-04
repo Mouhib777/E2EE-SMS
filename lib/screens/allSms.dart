9e78e54fcded3a422fbc3f58e66bdb0ce21badc2
Mouhib777 <mouhibsahbani@gmail.com>
2023-07-04 16:07:08 +0000

    c

--- lib/screens/allSms.dart
+++ lib/screens/allSms.dart
@@ -16,16 +16,6 @@ class AllSms extends StatefulWidget {
 }
 
 class _AllSmsState extends State<AllSms> {
-  final String encryptionPrefix = 'encrypted:';
-
-  String decryptAES(String cipherText, String key) {
-    final keyBytes = encrypt.Key.fromUtf8(key);
-    final iv = encrypt.IV.fromLength(16);
-    final encrypter = encrypt.Encrypter(encrypt.AES(keyBytes));
-    final decrypted = encrypter.decrypt64(cipherText, iv: iv);
-    return decrypted;
-  }
-
   Timer? timer; // Timer object to reload the page
 
   @override
@@ -67,29 +57,24 @@ class _AllSmsState extends State<AllSms> {
               itemCount: smsList!.length,
               itemBuilder: (context, index) {
                 final sender = smsList[index]['sender'];
-                final messageBody = smsList[index]['message_body'];
-                final decryptedBody = messageBody.startsWith(encryptionPrefix)
-                    ? decryptAES(messageBody.substring(encryptionPrefix.length),
-                        encryptionKey111)
-                    : messageBody;
+                final messageBody = decryptAES(
+                    smsList[index]['message_body'], encryptionKey111);
 
                 return InkWell(
                   onTap: () {
-                    pushNewScreenWithRouteSettings(
-                      context,
-                      screen: decryptionPage(
-                        title: sender,
-                        body: decryptedBody,
-                      ),
-                      settings: RouteSettings(),
-                      withNavBar: false,
-                      pageTransitionAnimation:
-                          PageTransitionAnimation.cupertino,
-                    );
+                    pushNewScreenWithRouteSettings(context,
+                        screen: decryptionPage(
+                          title: sender,
+                          body: messageBody,
+                        ),
+                        settings: RouteSettings(),
+                        withNavBar: false,
+                        pageTransitionAnimation:
+                            PageTransitionAnimation.cupertino);
                   },
                   child: ListTile(
                     title: Text(sender),
-                    subtitle: Text(decryptedBody),
+                    subtitle: Text(messageBody),
                     trailing: Icon(CupertinoIcons.chat_bubble),
                   ),
                 );
@@ -105,26 +90,28 @@ class _AllSmsState extends State<AllSms> {
     final databasePath = await getDatabasesPath();
     final database = await openDatabase(
       path.join(databasePath, 'sms_database.db'),
+      version: 1, // Specify the database version
+      onCreate: (db, version) {
+        // Create the 'sms_table' if it doesn't exist
+        db.execute('CREATE TABLE IF NOT EXISTS sms_table '
+            '(id INTEGER PRIMARY KEY, sender TEXT, message_body TEXT)');
+      },
     );
 
-    // Check if the 'sms_table' exists in the database
-    final tables = await database.query("sqlite_master",
-        where: "type = 'table' AND name = 'sms_table'");
-    if (tables.isEmpty) {
+    try {
+      // Query the 'sms_table' to fetch the SMS messages
+      final smsList = await database.query('sms_table');
+      return smsList;
+    } finally {
       await database.close();
-      return []; // Return an empty list when the table doesn't exist
     }
+  }
 
-    // Query the 'sms_table' only when it exists
-    final smsList = await database.query('sms_table');
-    await database.close();
-
-    if (smsList.isEmpty) {
-      return [
-        {'sender': 'No SMS found', 'message_body': 'No SMS found'}
-      ]; // Return a single entry when no SMS messages are found in the table
-    }
-
-    return smsList;
+  String decryptAES(String cipherText, String key) {
+    final keyBytes = encrypt.Key.fromUtf8(key);
+    final iv = encrypt.IV.fromLength(16);
+    final encrypter = encrypt.Encrypter(encrypt.AES(keyBytes));
+    final decrypted = encrypter.decrypt64(cipherText, iv: iv);
+    return decrypted;
   }
 }
