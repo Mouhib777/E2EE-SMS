5a4d3513869e17dbff9ea242ebc5a0005f0b2881
Mouhib777 <mouhibsahbani@gmail.com>
2023-07-11 00:04:51 +0000

    c

--- lib/screens/allSms.dart
+++ lib/screens/allSms.dart
@@ -45,7 +45,7 @@ class _AllSmsState extends State<AllSms> {
               icon: Icon(Icons.refresh))
         ],
       ),
-      floatingActionButton: AboutDialog(),
+      floatingActionButton: FloatingActionButton(onPressed: () {}),
       body: FutureBuilder<List<Map<String, dynamic>>>(
         future: smsListFuture, // Use the Future for fetching SMS messages
         builder: (context, snapshot) {
