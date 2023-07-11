94e3def3b3006bfaff7975f32823d14664d8df8c
Mouhib777 <mouhibsahbani@gmail.com>
2023-07-11 02:36:36 +0000

    c

--- lib/screens/allSms.dart
+++ lib/screens/allSms.dart
@@ -10,7 +10,6 @@ import 'package:sms_encry/screens/smsPage.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart' as path;
 import 'package:encrypt/encrypt.dart' as encrypt;
-import 'dart:async';
 
 class AllSms extends StatefulWidget {
   const AllSms({Key? key});
@@ -26,9 +25,9 @@ class _AllSmsState extends State<AllSms> {
   Future<List<Map<String, dynamic>>>?
       smsListFuture; // Future for fetching SMS messages
   List<Contact> _contacts = [];
-  List<Contact> _filteredContacts = [];
   bool _isLoading = false;
   TextEditingController _searchController = TextEditingController();
+  List<Map<String, dynamic>> _smsList = [];
 
   @override
   void initState() {
@@ -39,7 +38,7 @@ class _AllSmsState extends State<AllSms> {
       if (_searchController.text.isEmpty) {
         // If search text is empty, display all contacts
         setState(() {
-          _filteredContacts = _contacts;
+          _smsList = [];
         });
       }
     });
@@ -55,26 +54,12 @@ class _AllSmsState extends State<AllSms> {
 
       setState(() {
         _contacts = contacts.toList();
-        _filteredContacts = _contacts;
         _isLoading = false;
       });
     } else {
-      // Handle permissions not granted  void fetchContacts() async {
-      setState(() {
-        _isLoading = true;
-      });
+      // Handle permissions not granted
     }
-    setState(() {
-      _isLoading = false;
-    });
   }
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   // Fetch SMS messages when the page is initialized
-  //   smsListFuture = fetchSavedSMS();
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -106,19 +91,20 @@ class _AllSmsState extends State<AllSms> {
                   ),
                   Row(
                     children: [
-                      Text("Recepient:"),
+                      Text("Recipient:"),
                       SizedBox(
                         width: 5,
                       ),
                       SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.6,
-                          height: 25,
-                          child: TextField(
-                            keyboardType: TextInputType.number,
-                            onChanged: (value) {
-                              number = value;
-                            },
-                          )),
+                        width: MediaQuery.of(context).size.width * 0.6,
+                        height: 25,
+                        child: TextField(
+                          keyboardType: TextInputType.number,
+                          onChanged: (value) {
+                            number = value;
+                          },
+                        ),
+                      ),
                       SizedBox(
                         width: 1,
                       ),
@@ -144,11 +130,12 @@ class _AllSmsState extends State<AllSms> {
                     height: 10,
                   ),
                   Text(
-                    "Or , Select from your contacts",
+                    "Or, select from your contacts",
                     style: TextStyle(
-                        color: Colors.deepPurple,
-                        fontWeight: FontWeight.w500,
-                        fontSize: 15),
+                      color: Colors.deepPurple,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 15,
+                    ),
                   ),
                   SizedBox(
                     height: 20,
@@ -160,24 +147,16 @@ class _AllSmsState extends State<AllSms> {
                       autofocus: false,
                       controller: _searchController,
                       onChanged: (value) {
-                        // Filter the contacts based on the search text
-                        List<Contact> filteredContacts =
-                            _contacts.where((contact) {
-                          return contact.displayName
-                                  ?.toLowerCase()
-                                  .contains(value.toLowerCase()) ??
-                              false;
-                        }).toList();
-
                         setState(() {
-                          _filteredContacts = filteredContacts;
+                          _smsList = [];
                         });
                       },
                       decoration: InputDecoration(
                         labelText: 'Search',
                         prefixIcon: Icon(Icons.search),
                         border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15)),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
                       ),
                     ),
                   ),
@@ -186,36 +165,21 @@ class _AllSmsState extends State<AllSms> {
                   ),
                   Expanded(
                     child: ListView.builder(
-                      itemCount: _filteredContacts.length,
+                      itemCount: _smsList.length,
                       itemBuilder: (context, index) {
-                        Contact contact = _filteredContacts[index];
-                        return ListTile(
-                          leading: (contact.avatar == null ||
-                                  contact.avatar!.isEmpty)
-                              ? CircleAvatar(
-                                  backgroundImage:
-                                      AssetImage("assets/images/contact.png"),
-                                )
-                              : CircleAvatar(child: Text(contact.initials())),
-                          title: Text(
-                            contact.displayName ?? '',
-                            style: TextStyle(
-                                fontSize: 16.0, fontWeight: FontWeight.bold),
-                          ),
-                          subtitle: Text(
-                            contact.phones!.isNotEmpty
-                                ? contact.phones!.first.value ?? ''
-                                : '',
-                            style: TextStyle(fontSize: 14.0),
-                          ),
-                          trailing: Icon(CupertinoIcons.chat_bubble),
+                        final sender = _smsList[index]['sender'];
+                        final messageBody = decryptAES(
+                          _smsList[index]['message_body'],
+                          encryptionKey111,
+                        );
+
+                        return InkWell(
                           onTap: () {
                             pushNewScreenWithRouteSettings(
                               context,
-                              screen: smsPage(
-                                num: contact.phones!.isNotEmpty
-                                    ? contact.phones!.first.value ?? ''
-                                    : '',
+                              screen: decryptionPage(
+                                title: sender,
+                                body: messageBody,
                               ),
                               settings: RouteSettings(),
                               withNavBar: false,
@@ -223,6 +187,27 @@ class _AllSmsState extends State<AllSms> {
                                   PageTransitionAnimation.cupertino,
                             );
                           },
+                          child: ListTile(
+                            title: Text(sender),
+                            subtitle: Text(messageBody),
+                            trailing: IconButton(
+                              onPressed: () {
+                                final id = _smsList[index]['id'];
+                                if (id != null) {
+                                  deleteSMS(id).then((_) {
+                                    setState(() {
+                                      smsListFuture =
+                                          fetchSavedSMS(); // Refresh the SMS messages after deletion
+                                    });
+                                  });
+                                }
+                              },
+                              icon: Icon(
+                                CupertinoIcons.delete,
+                                color: Colors.red,
+                              ),
+                            ),
+                          ),
                         );
                       },
                     ),
@@ -245,15 +230,15 @@ class _AllSmsState extends State<AllSms> {
           centerTitle: true,
           actions: [
             IconButton(
-                onPressed: () {
-                  setState(() {
-                    smsListFuture = fetchSavedSMS(); // Refresh the SMS messages
-                  });
-                },
-                icon: Icon(Icons.refresh))
+              onPressed: () {
+                setState(() {
+                  smsListFuture = fetchSavedSMS(); // Refresh the SMS messages
+                });
+              },
+              icon: Icon(Icons.refresh),
+            ),
           ],
         ),
-        // floatingActionButton: FloatingActionButton(onPressed: () {}),
         body: FutureBuilder<List<Map<String, dynamic>>>(
           future: smsListFuture, // Use the Future for fetching SMS messages
           builder: (context, snapshot) {
@@ -267,50 +252,39 @@ class _AllSmsState extends State<AllSms> {
               // Display the SMS messages
               final smsList = snapshot.data;
 
-              return ListView.separated(
-                separatorBuilder: (BuildContext context, int index) => Divider(
-                  indent: 10,
-                  endIndent: 10,
-                ),
-                itemCount: smsList!.length,
-                itemBuilder: (context, index) {
-                  final sender = smsList[index]['sender'];
-                  final messageBody = decryptAES(
-                      smsList[index]['message_body'], encryptionKey111);
+              // Group SMS messages by sender
+              final groupedSmsList = _groupSmsBySender(smsList!);
 
-                  return InkWell(
-                    onTap: () {
-                      pushNewScreenWithRouteSettings(context,
-                          screen: decryptionPage(
-                            title: sender,
-                            body: messageBody,
-                          ),
-                          settings: RouteSettings(),
-                          withNavBar: false,
-                          pageTransitionAnimation:
-                              PageTransitionAnimation.cupertino);
-                    },
-                    child: ListTile(
-                      title: Text(sender),
-                      subtitle: Text(messageBody),
-                      trailing: IconButton(
-                        onPressed: () {
-                          final id = smsList[index]['id'];
-                          if (id != null) {
-                            deleteSMS(id).then((_) {
-                              setState(() {
-                                smsListFuture =
-                                    fetchSavedSMS(); // Refresh the SMS messages after deletion
-                              });
-                            });
-                          }
-                        },
-                        icon: Icon(
-                          CupertinoIcons.delete,
-                          color: Colors.red,
-                        ),
-                      ),
-                    ),
+              return ListView.builder(
+                itemCount: groupedSmsList.length,
+                itemBuilder: (context, index) {
+                  final sender = groupedSmsList[index]['sender'];
+                  final messages = groupedSmsList[index]['messages'];
+
+                  return ExpansionTile(
+                    title: Text(sender),
+                    children: messages
+                        .map<Widget>((sms) => ListTile(
+                              title: Text(decryptAES(
+                                  sms['message_body'], encryptionKey111)),
+                              trailing: IconButton(
+                                onPressed: () {
+                                  final id = sms['id'];
+                                  if (id != null) {
+                                    deleteSMS(id).then((_) {
+                                      setState(() {
+                                        smsListFuture = fetchSavedSMS();
+                                      });
+                                    });
+                                  }
+                                },
+                                icon: Icon(
+                                  CupertinoIcons.delete,
+                                  color: Colors.red,
+                                ),
+                              ),
+                            ))
+                        .toList(),
                   );
                 },
               );
@@ -319,6 +293,32 @@ class _AllSmsState extends State<AllSms> {
         ),
       ),
     );
+  }
+
+  List<Map<String, dynamic>> _groupSmsBySender(
+      List<Map<String, dynamic>> smsList) {
+    final groupedSmsMap = <String, List<Map<String, dynamic>>>{};
+
+    for (final sms in smsList) {
+      final sender = sms['sender'];
+
+      if (!groupedSmsMap.containsKey(sender)) {
+        groupedSmsMap[sender] = [];
+      }
+
+      groupedSmsMap[sender]!.add(sms);
+    }
+
+    final groupedSmsList = groupedSmsMap.entries
+        .map<Map<String, dynamic>>(
+          (entry) => {
+            'sender': entry.key,
+            'messages': entry.value,
+          },
+        )
+        .toList();
+
+    return groupedSmsList;
   }
 
   Future<List<Map<String, dynamic>>> fetchSavedSMS() async {
@@ -334,8 +334,12 @@ class _AllSmsState extends State<AllSms> {
     );
 
     try {
-      // Query the 'sms_table' to fetch the SMS messages
-      final smsList = await database.query('sms_table');
+      // Query the 'sms_table' to fetch the SMS messages sorted by the sender
+      final smsList = await database.query(
+        'sms_table',
+        orderBy: 'sender',
+      );
+
       return smsList;
     } finally {
       await database.close();
@@ -344,7 +348,7 @@ class _AllSmsState extends State<AllSms> {
 
   String decryptAES(String cipherText, String key) {
     if (cipherText.isEmpty || key.isEmpty) {
-      return ""; // Return an empty string if either cipherText or key is empty
+      return ''; // Return an empty string if either cipherText or key is empty
     }
 
     final keyBytes = encrypt.Key.fromUtf8(key);
